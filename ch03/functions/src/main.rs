56a95b4f2b5637cc111a92bb6fe3a621a0d1aa9a
Grant Ramsay <seapagan@gmail.com>
2023-10-23 19:27:41 +0100

    complete ch3 branches

--- ch03/functions/src/main.rs
+++ ch03/functions/src/main.rs
@@ -1,7 +1,29 @@
 fn main() {
+    println!("Hello, world!");
+
+    another_function(5);
+
     print_labeled_measurements(5, 'h');
+
+    let x = five();
+    println!("The value of x is: {x}");
+
+    let x = plus_one(5);
+    println!("The value of x is: {x}");
+}
+
+fn another_function(x: i32) {
+    println!("The value of x is: {x}");
 }
 
 fn print_labeled_measurements(x: i32, unit_label: char) {
-    println!("The value of x is: {x}{unit_label}");
+    println!("The labeled value of x is: {x}{unit_label}");
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
